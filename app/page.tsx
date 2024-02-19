cfb7ac83335e9115c84b916a310e85010991f73f
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-02-19 10:45:54 -0500

    feat: add quick search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import TripSearch from "./components/TripSearch";
+import QuickSearch from "./components/QuickSearch";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
+      <QuickSearch />
     </div>
   );
 }
