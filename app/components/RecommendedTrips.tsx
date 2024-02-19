60941f7724ae2c7a5a497d8ad055bf60f826d4bd
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-02-19 11:58:04 -0500

    feat: add RecommendedTrips component

---
+++ app/components/RecommendedTrips.tsx
@@ -0,0 +1,34 @@
+import TripItem from "@/components/TripItem";
+import { db } from "../_lib/prisma";
+import { Trip } from "@prisma/client";
+import React from "react";
+
+async function getTrips() {
+  const trips = await db.trip.findMany({});
+
+  return trips;
+}
+
+const RecommendedTrips = async () => {
+  const data = await getTrips();
+
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex items-center">
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+        <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">
+          Recommended Destinations
+        </h2>
+        <div className="w-full h-[1px] bg-grayLighter"></div>
+      </div>
+
+      <div className="flex flex-col items-center mt-5 lg:mt-12 gap-5 lg:flex-row gap lg:flex-wrap lg:justify-center lg:gap-10">
+        {data.map((trip: Trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedTrips;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
