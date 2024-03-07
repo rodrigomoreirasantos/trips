cac9732a4e0eb0fbb6d66fdfa1c0d56c9100d287
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-07 11:53:19 -0500

    feat: add type promisse to get route

--- app/api/user/[userId]/reservations/route.ts
+++ app/api/user/[userId]/reservations/route.ts
@@ -4,14 +4,9 @@ import { NextResponse } from "next/server";
 export async function GET(
   request: Request,
   { params: { userId } }: { params: { userId: string } }
-) {
+): Promise<void | Response> {
   if (!userId) {
-    return {
-      status: 400,
-      body: {
-        message: "Missing user id",
-      },
-    };
+    return new NextResponse("User ID is required", { status: 400 });
   }
 
   const reservations = await db.tripReservation.findMany({
