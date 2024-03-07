2201390393c1565a7cfea8baf321eb3d16dafa9e
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-07 11:24:24 -0500

    feat: add type for promisse delete to deploy

--- app/api/trips/reservation/[reservationId]/route.ts
+++ app/api/trips/reservation/[reservationId]/route.ts
@@ -4,14 +4,9 @@ import { NextResponse } from "next/server";
 export async function DELETE(
   _request: Request,
   { params: { reservationId } }: { params: { reservationId: string } }
-) {
+): Promise<void | Response> {
   if (!reservationId) {
-    return {
-      status: 400,
-      body: {
-        message: "Missing reservationId",
-      },
-    };
+    return new NextResponse("Reservation ID is required", { status: 400 });
   }
 
   const reservation = await db.tripReservation.delete({
