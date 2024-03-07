35c4283db3791e90022ffbeb9fccad1781e50843
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-07 11:46:39 -0500

    feat: remove delete type

--- app/api/trips/reservation/[reservationId]/route.ts
+++ app/api/trips/reservation/[reservationId]/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function DELETE(
   _request: Request,
   { params: { reservationId } }: { params: { reservationId: string } }
-): Promise<void | Response> {
+) {
   if (!reservationId) {
     return new NextResponse("Reservation ID is required", { status: 400 });
   }
