e8180667dd1b573b4a322465704d4e8764f7c141
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-08 11:30:06 -0500

    feat: add force dynamic in home

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
 import RecommendedTrips from "./components/RecommendedTrips";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div>
