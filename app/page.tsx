09f189a3f4352996f45a2f174e6c5582061c462b
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-08 11:22:43 -0500

    feat: add correct url to route

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
 import RecommendedTrips from "./components/RecommendedTrips";
-import { Suspense } from "react";
 
 export default function Home() {
   return (
--- app/trips/search/page.tsx
+++ app/trips/search/page.tsx
@@ -13,7 +13,7 @@ const Trips = () => {
   useEffect(() => {
     const fetchTrips = async () => {
       const response = await fetch(
-        `/api/trips/search?text=${
+        `/trips/search?text=${
           searchParams.get("text") ?? ""
         }&startDate=${searchParams.get("startDate")}&budget=${searchParams.get(
           "budget"
