8f414d735955ed5e5565a9cb11e9d94b6f6f7b74
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-08 11:34:06 -0500

    feat: add suspense in Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
 import RecommendedTrips from "./components/RecommendedTrips";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <div>
-      <TripSearch />
-      <QuickSearch />
-      <RecommendedTrips />
-    </div>
+    <Suspense>
+      <div>
+        <TripSearch />
+        <QuickSearch />
+        <RecommendedTrips />
+      </div>
+    </Suspense>
   );
 }
